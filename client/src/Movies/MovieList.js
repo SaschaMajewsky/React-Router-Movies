f319418e2e6f72a3738c100bc3d4c17c04a28c72
Sascha <sascha.majewsky93@gmx.de>
2019-06-11 02:10:17 +0200

    added in JSX inline styling so that movies won't be displayed as a visited URL

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -35,7 +35,7 @@ export default class MovieList extends Component {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars, id} = movie;
   return (
-    <Link to={`/movies/${id}`}>
+    <Link style={{textDecoration: "none", color: "blue"}} to={`/movies/${id}`}>
     <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
